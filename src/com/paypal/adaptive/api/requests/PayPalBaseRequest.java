8e380034e0c820e3de82080a3cddc2f5577f1c1c
Brandon Donnelson <branflake2267@gmail.com>
2012-04-11 09:56:50 -0700

    Auto formating in IntelliJ

--- src/com/paypal/adaptive/api/requests/PayPalBaseRequest.java
+++ src/com/paypal/adaptive/api/requests/PayPalBaseRequest.java
@@ -1,24 +1,5 @@
 package com.paypal.adaptive.api.requests;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
 import com.paypal.adaptive.core.APICredential;
 import com.paypal.adaptive.core.EndPointUrl;
 import com.paypal.adaptive.core.RequestEnvelope;
@@ -27,21 +8,31 @@ import com.paypal.adaptive.exceptions.InvalidResponseDataException;
 import com.paypal.adaptive.exceptions.MissingParameterException;
 import com.paypal.adaptive.exceptions.RequestFailureException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public abstract class PayPalBaseRequest {
 
-	private static final Logger log = Logger.getLogger(PayPalBaseRequest.class.getName());
+    private static final Logger log = Logger.getLogger(PayPalBaseRequest.class.getName());
 
-	protected ServiceEnvironment env;
-	protected RequestEnvelope requestEnvelope;
-	public static int HTTP_CONNECTION_TIMEOUT = 15000;
-	public static int HTTP_READ_TIMEOUT = 7000;
-	public static boolean DISABLE_SSL_CERT_CHECK = false;
+    protected ServiceEnvironment env;
+    protected RequestEnvelope requestEnvelope;
+    public static int HTTP_CONNECTION_TIMEOUT = 15000;
+    public static int HTTP_READ_TIMEOUT = 7000;
+    public static boolean DISABLE_SSL_CERT_CHECK = false;
 
-	protected String makeRequest(APICredential credentialObj, String apiMethod, String postData)
-	throws MissingParameterException, InvalidResponseDataException, 
-	RequestFailureException, IOException{
-		String responseString = "";
-		try {
+    protected String makeRequest(APICredential credentialObj, String apiMethod, String postData)
+            throws MissingParameterException, InvalidResponseDataException,
+            RequestFailureException, IOException {
+        String responseString = "";
+        try {
 //			if(DISABLE_SSL_CERT_CHECK){
 //				// Create a trust manager that does not validate certificate chains 
 //				TrustManager[] trustAllCerts = new TrustManager[] { 
@@ -62,53 +53,53 @@ public abstract class PayPalBaseRequest {
 //					}
 //				});
 //			}
-			URL url = new URL(EndPointUrl.get(this.env) + apiMethod);
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setDoOutput(true);
-			// set timeouts
-			connection.setConnectTimeout(HTTP_CONNECTION_TIMEOUT);
-			connection.setReadTimeout(HTTP_READ_TIMEOUT);
-			// method is always POST
-			connection.setRequestMethod("POST");
-			// set HTTP headers
-			connection.setRequestProperty("X-PAYPAL-SECURITY-USERID", credentialObj.getAPIUsername());
-			connection.setRequestProperty("X-PAYPAL-SECURITY-PASSWORD", credentialObj.getAPIPassword());
-			connection.setRequestProperty("X-PAYPAL-SECURITY-SIGNATURE", credentialObj.getSignature());
-			connection.setRequestProperty("X-PAYPAL-REQUEST-DATA-FORMAT", "NV");
-			connection.setRequestProperty("X-PAYPAL-RESPONSE-DATA-FORMAT", "NV");
-			connection.setRequestProperty("X-PAYPAL-APPLICATION-ID", credentialObj.getAppId());
-			connection.setRequestProperty("X-PAYPAL-REQUEST-SOURCE", "GAE-JAVA_Toolkit");
+            URL url = new URL(EndPointUrl.get(this.env) + apiMethod);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            // set timeouts
+            connection.setConnectTimeout(HTTP_CONNECTION_TIMEOUT);
+            connection.setReadTimeout(HTTP_READ_TIMEOUT);
+            // method is always POST
+            connection.setRequestMethod("POST");
+            // set HTTP headers
+            connection.setRequestProperty("X-PAYPAL-SECURITY-USERID", credentialObj.getAPIUsername());
+            connection.setRequestProperty("X-PAYPAL-SECURITY-PASSWORD", credentialObj.getAPIPassword());
+            connection.setRequestProperty("X-PAYPAL-SECURITY-SIGNATURE", credentialObj.getSignature());
+            connection.setRequestProperty("X-PAYPAL-REQUEST-DATA-FORMAT", "NV");
+            connection.setRequestProperty("X-PAYPAL-RESPONSE-DATA-FORMAT", "NV");
+            connection.setRequestProperty("X-PAYPAL-APPLICATION-ID", credentialObj.getAppId());
+            connection.setRequestProperty("X-PAYPAL-REQUEST-SOURCE", "GAE-JAVA_Toolkit");
 
-			if(log.isLoggable(Level.INFO))
-        log.info("Connection: " + connection);
-			
-			OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-			writer.write(postData);
-			writer.close();
+            if (log.isLoggable(Level.INFO))
+                log.info("Connection: " + connection);
 
-			if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-				String inputLine;
-				BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
-				while ((inputLine = reader.readLine()) != null) {
-					responseString += inputLine;
-				}
-				reader.close();
-				if(responseString.length() <= 0){
-					throw new InvalidResponseDataException(responseString);
-				}
-				if(log.isLoggable(Level.INFO))
-					log.info("Received Response: " + responseString);
-			} else {
-				// Server returned HTTP error code.
-				throw new RequestFailureException(connection.getResponseCode());
-			}
-		} catch (MalformedURLException e) {
-			// ...
-			throw e;
-		} catch (IOException e) {
-			// ...
-			throw e;
-		} 
-		return responseString;
-	}
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(postData);
+            writer.close();
+
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                String inputLine;
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+                while ((inputLine = reader.readLine()) != null) {
+                    responseString += inputLine;
+                }
+                reader.close();
+                if (responseString.length() <= 0) {
+                    throw new InvalidResponseDataException(responseString);
+                }
+                if (log.isLoggable(Level.INFO))
+                    log.info("Received Response: " + responseString);
+            } else {
+                // Server returned HTTP error code.
+                throw new RequestFailureException(connection.getResponseCode());
+            }
+        } catch (MalformedURLException e) {
+            // ...
+            throw e;
+        } catch (IOException e) {
+            // ...
+            throw e;
+        }
+        return responseString;
+    }
 }
